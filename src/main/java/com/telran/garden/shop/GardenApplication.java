adce4fd10340c02ce46448e465ecad2d2d3c2912
VoytovychM <mary.voytovych@gmail.com>
2024-08-07 20:17:02 +0200

    initial commit

---
+++ src/main/java/com/telran/garden/shop/GardenApplication.java
@@ -0,0 +1,15 @@
+package com.telran.garden.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@EnableScheduling
+//@EnableAsync
+public class GardenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GardenApplication.class, args);
+	}
+
+}
